05bc71440b528a6f6b3afedf0b8d71bc42649e60
obernardocomercial@gmail.com <obernardocomercial@gmail.com>
2026-07-22 11:51:58 -0300

    Desafios Aula #6

---
+++ Aula #6/teste01.py
@@ -0,0 +1,9 @@
+Booleano = bool(input("Digite um valor booleano (T/F): "))
+Inteiro = int(input("Digite um valor inteiro: "))
+Real = float(input("Digite um valor real: "))
+String = str(input("Digite um valor string: "))
+
+print(type(Booleano))
+print(type(Inteiro))
+print(type(Real))
+print(type(String))
---
+++ Aula #6/teste02.py
@@ -0,0 +1,5 @@
+n = bool(input('Digite um valor: '))
+print(n)
+
+n1 = input('Digite algo: ')
+print(n1.isnumeric())
